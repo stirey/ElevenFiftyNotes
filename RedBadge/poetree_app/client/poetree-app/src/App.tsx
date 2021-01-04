d0fdad1f0d1a9cf64555f644e99fc8d6d5e1ee23
Sarah Tirey <sarah@email.com>
2021-01-03 19:16:03 -0500

    first commit

---
+++ RedBadge/poetree_app/client/poetree-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// what follows the colon is the type that we are assigning to App.
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
