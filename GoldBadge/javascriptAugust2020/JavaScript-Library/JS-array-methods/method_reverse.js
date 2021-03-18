08cd8b8f840274e89023e8e7d0491017662e9175
Sarah Tirey <sarahtirey@gmail.com>
2021-03-18 19:54:39 -0400

    added reverse method notes

---
+++ GoldBadge/javascriptAugust2020/JavaScript-Library/JS-array-methods/method_reverse.js
@@ -0,0 +1,4 @@
+//The reverse() method reverses an array in place. By reversing we mean that the function will transpose the elements of the array, the first element will become the last, and the last the first element. This operation will mutate the original array and return a reference to the same.
+//a.reverse()
+
+console.log([1, 2, 3].reverse())
