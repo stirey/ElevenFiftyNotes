3580cd835ca141be13bb7a4aba6971fc8f21cdaf
Sarah Tirey <sarahtirey@gmail.com>
2021-03-18 19:51:46 -0400

    added includes method notes

---
+++ GoldBadge/javascriptAugust2020/JavaScript-Library/JS-array-methods/method_includes.js
@@ -0,0 +1,5 @@
+//The includes() method determines whether an array includes a certain value among its entries, returning true or false. Note that the method includes() is case-sensitive when comparing strings and characters.
+//arr.includes(valueToFind[, fromIndex])
+
+console.log([1, 2, 3].includes(2))
+console.log([1, 2, 3].includes(4))
